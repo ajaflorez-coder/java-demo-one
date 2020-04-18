b8df54c7a95606435f609706b0326f30719282f0
Piero <u20161c138@upc.edu.pe>
2020-04-18 15:12:51 -0500

    Se agregó constructor, get y set en la clase Piero

--- src/pe/edu/upc/covid19/model/entity/Piero.java
+++ src/pe/edu/upc/covid19/model/entity/Piero.java
@@ -4,4 +4,25 @@ public class Piero {
     private String nombre;
     private int dni;
 
+    public Piero(String nombre, int dni) {
+        this.setNombre(nombre);
+        this.setDni(dni);
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 }
