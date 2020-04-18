48198244c8b7d442e559433651dbe52a84b0e04d
Galatenea98 <elizabethtenayucra@gmail.com>
2020-04-18 15:25:29 -0500

    Firts commit Elizabeth

---
+++ src/two/Elizabeth.java
@@ -0,0 +1,25 @@
+package two;
+
+public class Elizabeth{
+	private int Id;
+	private String Apellido;
+
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        this.Id = id;
+    }
+
+    public void setApellido(String apellido) {
+        this.Apellido = apellido;
+    }
+    
+
+}
