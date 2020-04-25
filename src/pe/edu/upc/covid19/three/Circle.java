869b138d2e5ace29c8b64263339cb3df64ab4342
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-04-25 15:35:16 -0500

    Add new feature Three

---
+++ src/pe/edu/upc/covid19/three/Circle.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.covid19.three;
+
+public class Circle implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Drawing circle");
+	}
+
+}
---
+++ src/pe/edu/upc/covid19/three/Shape.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.covid19.three;
+
+public interface Shape {
+	void draw();
+}
